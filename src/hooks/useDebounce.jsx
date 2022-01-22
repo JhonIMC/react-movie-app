d963acf0685b42e64466cda80715517d7b282864
Jonathan Monroy <jonathan.monroy123ap@gmail.com>
2022-01-22 17:05:06 -0600

    index.html actualizado

--- src/hooks/useDebounce.jsx
+++ src/hooks/useDebounce.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react/cjs/react.development";
+import { useEffect, useState } from "react";
 
 export function useDebounce(value, delay) {
     // State and setters for debounced value
